9da7ffbae2dd2e83e4142727a208a7c5670f4111
khaerul99 <khaerul.r.23@gmil.com>
2023-02-18 20:22:01 +0700

    update

---
+++ resources/views/landing/master.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('landing.header')
+</head>
+
+<body>
+    <!-- Topbar Start -->
+    @include('landing.topbar')
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    @include('landing.navbar')
+    <!-- Navbar End -->
+
+
+    <!-- Featured Start -->
+   @include('landing.feature')
+    <!-- Featured End -->
+
+
+    <!-- Categories Start -->
+    @include('landing.categories')
+    <!-- Categories End -->
+
+
+    <!-- Offer Start -->
+    @include('landing.offer')
+    <!-- Offer End -->
+
+
+    <!-- Products Start -->
+    @include('landing.products')
+    <!-- Products End -->
+
+
+    <!-- Subscribe Start -->
+    @include('landing.subscribe')
+    <!-- Subscribe End -->
+
+
+    <!-- Products Start -->
+    @include('landing.products')
+    <!-- Products End -->
+
+
+    <!-- Vendor Start -->
+    @include('landing.vendor')
+    <!-- Vendor End -->
+
+
+    <!-- Footer Start -->
+    @include('landing.footer')
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+    <!-- JavaScript Libraries -->
+    @include('landing.js')
+</body>
+
+</html>
